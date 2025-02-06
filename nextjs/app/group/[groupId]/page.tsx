02689a9c270146adf2522f3987d9fea66b82fae0
NurRobin <grambrobin@gmail.com>
2025-02-06 09:28:52 +0000

    Add GroupPageClient and layout components for group details display

--- nextjs/app/group/[groupId]/page.tsx
+++ nextjs/app/group/[groupId]/page.tsx
@@ -1,30 +1,53 @@
 // src/app/group/[groupId]/page.tsx
 import React from 'react';
-import SwiperCard from '../../../components/SwiperCard';
+import GroupPageClient from './GroupPageClient';
+import pb from '@/lib/pocketbase';
 
-const GroupPage: React.FC = () => {
-  // Replace with actual data fetching logic
-  const cardsData = [
-    { id: '1', title: 'Option 1', description: 'Description 1' },
-    { id: '2', title: 'Option 2', description: 'Description 2' },
-  ];
+async function getGroupWithMembers(groupId: string): Promise<Group> {
+  console.log('getting group with groupId', groupId);
+  const group = await pb.collection('groups').getOne(groupId, {
+    expand: 'members',
+  });
 
-  const handleSwipe = async (direction: string, cardId: string) => {
-    // Replace 'groupId' with actual group ID
-    await fetch('/api/votes', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ groupId: 'groupId', cardId, vote: direction }),
-    });
+  console.log('we got the group', group);
+
+  return {
+    id: group.id,
+    created_by: group.created_by,
+    members: (group.expand?.members || []).map((user: any) => ({
+      id: user.id,
+      email: user.email,
+      display_name: user.display_name,
+      created: user.created,
+      updated: user.updated,
+    })) as User[],
+    name: group.name,
+    description: group.description,
+    created: group.created,
+    updated: group.updated,
   };
+}
 
-  return (
-    <div>
-      {cardsData.map((card) => (
-        <SwiperCard key={card.id} data={card} onSwipe={handleSwipe} />
-      ))}
-    </div>
-  );
-};
+export default async function GroupPage({ params }: { params: { groupId: string } }) {
+  const group = await getGroupWithMembers(params.groupId);
 
-export default GroupPage;
+  return <GroupPageClient group={group} members={group.members} />;
+}
+
+interface Group {
+  id: string;
+  created_by: string;
+  members: User[];
+  name: string;
+  description: string;
+  created: string;
+  updated: string;
+}
+
+interface User {
+  id: string;
+  email: string;
+  display_name: string;
+  created: string;
+  updated: string;
+}
